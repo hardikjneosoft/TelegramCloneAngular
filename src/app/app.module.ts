260858b6e92d332b5d38988dc21071d36040f8e6
neosoft <hardik.j@neosoftmail.com>
2024-08-16 14:45:50 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    NavbarLinkComponent   // components, pipes
+    NavbarLinkComponent,
+    HomeComponent   // components, pipes
   ],
   imports: [
     BrowserModule,  // dependenices modules 
