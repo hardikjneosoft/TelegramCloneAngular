8ab40faa6b76ef00b0388b93d2cca370cfa0d7c0
neosoft <hardik.j@neosoftmail.com>
2024-08-22 11:07:49 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarLinkComponent } from './navbar-link/navbar-link.component';
+import { NavbarLinkComponent } from './navbar/navbar-link/navbar-link.component';
 import { HomeComponent } from './home/home.component';
 import { ChatComponent } from './chat/chat.component';
 import { FormsModule } from '@angular/forms';
